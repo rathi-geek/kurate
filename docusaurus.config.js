422cb932ddf3c8f3cf3ab6422be8ccada4c98a1a
suresh konakanchi <konakanchisuresh950@gmail.com>
2024-10-07 14:22:18 +0530

    Add @docusaurus/theme-mermaid to package.json and docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,6 +17,10 @@ const config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
+  markdown: {
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
   presets: [
     [
       'classic',
