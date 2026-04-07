8a6df475b4a1995f4c1ae73b1110b29e45da3c0e
Ankur Rathi <ankurrathi@geekyants.com>
2026-04-07 16:36:12 +0530

    Increase group name limit and remove uniquness from gorup name

--- supabase/migrations/20260407_drop_group_name_unique.sql
+++ supabase/migrations/20260407_drop_group_name_unique.sql
@@ -1,2 +1,5 @@
 -- Allow multiple groups with the same name (different users can create "Book Club", etc.)
 ALTER TABLE public.conversations DROP CONSTRAINT IF EXISTS conversations_group_name_key;
+
+-- Increase group name character limit from 20 to 50
+ALTER TABLE public.conversations ALTER COLUMN group_name TYPE VARCHAR(50);
